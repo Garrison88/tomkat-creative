5b0c20db3afa501f138288415f4a75473e48e671
Garrison Siberry <garrison.siberry@gmail.com>
2018-03-23 22:23:57 +0000

    add gallery, begin gallery details

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express          = require("express"),
     app              = express()
-    
+
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 
@@ -14,9 +14,27 @@ app.get('/work', function(req, res) {
     res.render('pages/work');
 });
 
+// // SHOW CAMPGROUND route
+// app.get("/:id", function(req, res){
+//     //find the campground with provided ID
+//     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log(foundCampground)
+//             //render show template with that campground
+//             res.render("campgrounds/show", {campground: foundCampground})
+//         }
+//     })
+// })
+
 // contact page 
 app.get('/contact', function(req, res) {
     res.render('pages/contact');
+});
+
+app.get('/gallery-detail/cell1', function(req, res) {
+    res.render('pages/gallery-detail/cell1');
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
