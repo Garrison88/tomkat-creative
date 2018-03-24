02f822d2ab0f62620e3ae22d97e8b6ad9e444a30
Garrison Siberry <garrison.siberry@gmail.com>
2018-03-24 19:19:25 +0000

    add RESTful routes, functional gallery page - all back-end stuff

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var express          = require("express"),
-    app              = express()
-
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser")
+    // mongoose   = require("mongoose")
+    // Project    = require("./models/project")
+    
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({extended: true}))
+    
+var projects = [
+        {title: 'BlueGirl', imageUrl: '/img/blue-girl.png', description: 'Beautiful lady with crazy hair!', thumbUrl: '/img/blue-girl.png'},
+        {title: 'BlackGirl', imageUrl: '/img/black-girl.png', description: 'Dancing gracefully', thumbUrl: '/img/black-girl.png'},
+        {title: 'Zebra', imageUrl: '/img/bullshittime.png', description: 'Stretching down to eat?', thumbUrl: '/img/bullshittime.png'},
+        {title: 'Cloud', imageUrl: '/img/cloud.png', description: 'Little guy raining on everything', thumbUrl: '/img/cloud.png'},
+        {title: 'Dogfish', imageUrl: '/img/dogfish.gif', description: 'Dog pooping itself out', thumbUrl: '/img/dogfish.gif'},
+        {title: 'Dolphin', imageUrl: '/img/dolphintest.gif', description: 'Baaaayyyybbyyy dolphin...', thumbUrl: '/img/dolphintest.gif'},
+        {title: 'Header', imageUrl: '/img/header.png', description: 'You have seen this before', thumbUrl: '/img/header.png'},
+        {title: 'WhiteGirl', imageUrl: '/img/white-girl.png', description: 'This girl needs a gun', thumbUrl: '/img/white-girl.png'},
+        {title: 'YellowZebra', imageUrl: '/img/yellow-zebra.png', description: 'A yellow variant on a good thing', thumbUrl: '/img/yellow-zebra.png'}
+        ]
 
-// index page 
+// INDEX route 
 app.get('/', function(req, res) {
-    res.render('pages/index');
-});
+    res.render('home')
+})
 
-// work page 
-app.get('/work', function(req, res) {
-    res.render('pages/work');
-});
+// gallery INDEX route 
+app.get('/gallery/index', function(req, res) {
+    res.render('gallery/index', {projects: projects})
+})
 
-// // SHOW CAMPGROUND route
-// app.get("/:id", function(req, res){
-//     //find the campground with provided ID
-//     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             console.log(foundCampground)
-//             //render show template with that campground
-//             res.render("campgrounds/show", {campground: foundCampground})
-//         }
-//     })
-// })
+// gallery SHOW route
+app.get('/gallery/:title', function(req, res){
+    projects.forEach(function(projects){
+        if (projects.title === req.params.title) {
+          res.render("gallery/show", {projects: projects}) 
+        }
+    })
+})
 
-// contact page 
+// Contact route
 app.get('/contact', function(req, res) {
-    res.render('pages/contact');
-});
-
-app.get('/gallery-detail/cell1', function(req, res) {
-    res.render('pages/gallery-detail/cell1');
-});
+    res.render('contact')
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The Server Has Started!")
---
+++ models/project.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var projectSchema = new mongoose.Schema({
+   title: String,
+   image: String,
+   credits: String,
+   description: String,
+   imageUrl: String,
+   thumbUrl: String
+});
+
+module.exports = mongoose.model("Project", projectSchema);
