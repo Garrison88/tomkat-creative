9731b2d9f630838f85a3d6ff21f336c765908bd5
Garrison Siberry <garrison.siberry@gmail.com>
2018-03-26 03:24:26 +0000

    bring in first batch of images and links

--- projects.js
+++ projects.js
@@ -1,65 +1,114 @@
 var projects = [
         // ** ANIMATION **
-    {
+        {
+        type: 'animation',
+        title: 'Abstract Film',
+        image: 'sexy-gif.gif',
+        description: 'Abstract sexy gif',
+        thumb: 'cloud.png'
+    },
+        {
         type: 'animation',
         title: 'Dolphin',
-        imageUrl: '/img/anim_dolphin.gif',
-        description: 'Baaaayyyybbyyy dolphin...',
-        thumbUrl: '/img/anim_dolphin.gif'
+        image: 'anim_dolphin.gif',
+        description: 'Baaaayyyybbyyy dolphin...!',
+        thumb: 'dolphin.gif'
     },
-    {
+        {
+        type: 'animation',
+        title: 'Demo Reel',
+        description: 'Demo reel',
+        thumb: 'demo-reel.png',
+        videoEmbedUrl: 'player.vimeo.com/video/259600005'
+    },
+        {
+        type: 'animation',
+        title: 'Powerstick 1',
+        description: 'Wherever Pete goes, he is never without his trusty Powerstick. Don\'t get stuck without your portable devices. Carry power with you just like Pete!',
+        thumb: 'powerstick.png',
+        videoEmbedUrl: 'www.youtube.com/embed/bsuD9j7QxKQ'
+    },
+        {
+        type: 'animation',
+        title: 'Powerstick 2',
+        description: 'Stay powered up just like Pete! Watch Pete charge his mobile devices with his solar-powered PowerBinder through rain or shine.',
+        thumb: 'powerstick.png',
+        videoEmbedUrl: 'www.youtube.com/embed/eTu6m1PpEI0'
+    },
+        {
+        type: 'animation',
+        title: 'Powerstick 3',
+        description: 'Portable charging never sounded so good! The all-new PowerSound II is both a portable charger and Bluetooth speaker.',
+        thumb: 'powerstick.png',
+        videoEmbedUrl: 'www.youtube.com/embed/oNGiLFF9dYY'
+    },
+        {
         type: 'animation',
         title: 'Birthday Wishes',
-        // imageUrl: '/img/cloud.png',
         description: 'Birthday Wishes for JibJab Facebook App',
-        thumbUrl: '/img/cloud.png',
-        videoEmbedUrl: 'https://player.vimeo.com/video/259804841'
+        thumb: 'birthday-wishes.png',
+        videoEmbedUrl: 'player.vimeo.com/video/259804841'
     },
         {
         type: 'animation',
         title: 'Enhanced Imagination',
-        // imageUrl: '/img/cloud.png',
         description: 'Animation spot for Enhanced Imagination',
-        thumbUrl: '/img/cloud.png',
-        videoEmbedUrl: 'https://player.vimeo.com/video/259804372'
+        thumb: 'enhanced-imagination.png',
+        videoEmbedUrl: 'player.vimeo.com/video/259804372'
     },
-    {
+        {
         type: 'animation',
         title: 'Ground Zero',
-        // imageUrl: '/img/cloud.png',
         description: 'Commercial spot for Ground Zero (Toronto)',
-        thumbUrl: '/img/cloud.png',
-        videoEmbedUrl: 'https://player.vimeo.com/video/259804261'
-    },
-    {
-        type: 'animation',
-        title: 'Dogfish',
-        imageUrl: '/img/dogfish.gif',
-        description: 'Dog pooping itself out',
-        thumbUrl: '/img/dogfish.gif'
+        thumb: 'ground-zero.png',
+        videoEmbedUrl: 'player.vimeo.com/video/259804261'
     },
         // ** ILLUSTRATION **
-    {
-        type: 'animation',
-        title: 'Blue Girl',
-        imageUrl: '/img/blue-girl.png',
-        description: 'In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content',
-        thumbUrl: '/img/blue-girl.png'
-    },
-    {
+        {
         type: 'illustration',
-        title: 'Zebra',
-        imageUrl: '/img/bullshittime.png',
-        description: 'Stretching down to eat?',
-        thumbUrl: '/img/bullshittime.png'
+        title: 'Berry Boat',
+        image: 'berry-boat-1.png',
+        description: 'Children\'s book',
+        thumb: 'berry-boat.png'
+    },
+        {
+        type: 'illustration',
+        title: 'Handsome Man',
+        image: 'gerby-1.jpg',
+        description: 'Disney-fied',
+        thumb: 'gerby.png'
+    },
+        {
+        type: 'illustration',
+        title: 'Animals',
+        image: 'animals-1.png',
+        description: 'Wild animals',
+        thumb: 'animals.png'
     },
         // ** WEB **
-    {
+        {
+        type: 'web',
+        title: 'Barking Teacup',
+        image: 'barking-teacup-1.png',
+        siteUrl: 'https://twitter.com/barkingteacup',
+        description: 'Barking Teacup logo',
+        thumb: 'barking-teacup.png'
+    },
+        {
+        type: 'web',
+        title: 'Big World Small Budget',
+        image: 'bwsb.png',
+        siteUrl: 'http://bigworldsmallbudget.com/',
+        description: 'Big World Small Budget logo',
+        thumb: 'bwsb.png'
+    },
+        {
         type: 'web',
         title: 'DawnAyer.com',
-        imageUrl: '/img/dawn-ayer.png',
+        image: 'dawn-ayer.png',
+        siteUrl: 'http://dawnayer.com/',
         description: 'Commissioned by dawn ayer COMMUNICATIONS to design and develop a website promoting their services',
-        thumbUrl: '/img/thumbnails/dawn-ayer.png'
+        thumb: 'dawn-ayer.png'
     },
 ]
 
