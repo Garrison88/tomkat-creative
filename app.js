3a65e1711a150a5ee453d3a56e464c3ead86af87
Garrison Siberry <garrison.siberry@gmail.com>
2018-03-26 00:05:04 +0000

    continue to dry up code, added embedded vimeo videos

--- app.js
+++ app.js
@@ -1,81 +1,49 @@
-var express    = require("express"),
+var express    = require('express'),
     app        = express(),
-    bodyParser = require("body-parser")
+    bodyParser = require('body-parser'),
+    projects   = require('./projects')    
     
 app.set("view engine", "ejs")
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: true}))
-    
-var projects = [
-    {
-        type: 'animation',
-        title: 'Dolphin',
-        imageUrl: '/img/anim_dolphin.gif',
-        description: 'Baaaayyyybbyyy dolphin...',
-        thumbUrl: '/img/anim_dolphin.gif'
-    },
-    {
-        type: 'animation',
-        title: 'Blue Girl',
-        imageUrl: '/img/blue-girl.png',
-        description: 'In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content. Replacing the actual content with placeholder text allows designers to design the form of the content before the content itself has been produced.',
-        thumbUrl: '/img/blue-girl.png'
-    },
-    {
-        type: 'web',
-        title: 'DawnAyer.com',
-        imageUrl: '/img/dawn-ayer.png',
-        description: 'Commissioned by dawn ayer COMMUNICATIONS to design and develop a website promoting their services',
-        thumbUrl: '/img/thumbnails/dawn-ayer.png'
-    },
-    {
-        type: 'illustration',
-        title: 'Zebra',
-        imageUrl: '/img/bullshittime.png',
-        description: 'Stretching down to eat?',
-        thumbUrl: '/img/bullshittime.png'
-    },
-    {
-        type: 'animation',
-        title: 'Dogfish',
-        imageUrl: '/img/dogfish.gif',
-        description: 'Dog pooping itself out',
-        thumbUrl: '/img/dogfish.gif'
-    },
-]
 
 // INDEX route 
 app.get('/', function(req, res) {
-    res.render('home')
+    res.render('home', {homeActive: 'active',
+                        galleryActive: '',
+                        contactActive: '',
+                        background: 'home'})
 })
 
 // gallery INDEX route 
 app.get('/gallery/:type/index', function(req, res) {
-    switch (req.params.type) {
-        case 'animation':
-            res.render('./gallery/index', {type: req.params.type, projects: projects})
-            break;
-        case 'illustration':
-            res.render('./gallery/index', {type: req.params.type, projects: projects})
-            break;
-        case 'web':
-            res.render('./gallery/index', {type: req.params.type, projects: projects})
-            break;
-    }
+    res.render('./gallery/index', {type: req.params.type,
+                                   project: projects,
+                                   homeActive: '',
+                                   galleryActive: 'active',
+                                   contactActive: '',
+                                   background: 'gallery'})
 })
 
 // gallery SHOW route
 app.get('/gallery/:type/:title', function(req, res){
     projects.forEach(function(project){
         if (project.title === req.params.title) {
-            res.render('gallery/' + req.params.type + '/show', {project: project}) 
-        }
+            res.render('gallery/' + req.params.type + '/show', {project: project,
+                                                                homeActive: '',
+                                                                galleryActive: 'active',
+                                                                contactActive: '',
+                                                                background: 'gallery-show'})
+        }                                         
     })
 })
 
 // Contact route
 app.get('/contact', function(req, res) {
-    res.render('contact')
+    res.render('contact', {homeActive: '',
+                           galleryActive: '',
+                           contactActive: 'active',
+                           background: 'contact'})
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
---
+++ projects.js
@@ -0,0 +1,66 @@
+var projects = [
+        // ** ANIMATION **
+    {
+        type: 'animation',
+        title: 'Dolphin',
+        imageUrl: '/img/anim_dolphin.gif',
+        description: 'Baaaayyyybbyyy dolphin...',
+        thumbUrl: '/img/anim_dolphin.gif'
+    },
+    {
+        type: 'animation',
+        title: 'Birthday Wishes',
+        // imageUrl: '/img/cloud.png',
+        description: 'Birthday Wishes for JibJab Facebook App',
+        thumbUrl: '/img/cloud.png',
+        videoEmbedUrl: 'https://player.vimeo.com/video/259804841'
+    },
+        {
+        type: 'animation',
+        title: 'Enhanced Imagination',
+        // imageUrl: '/img/cloud.png',
+        description: 'Animation spot for Enhanced Imagination',
+        thumbUrl: '/img/cloud.png',
+        videoEmbedUrl: 'https://player.vimeo.com/video/259804372'
+    },
+    {
+        type: 'animation',
+        title: 'Ground Zero',
+        // imageUrl: '/img/cloud.png',
+        description: 'Commercial spot for Ground Zero (Toronto)',
+        thumbUrl: '/img/cloud.png',
+        videoEmbedUrl: 'https://player.vimeo.com/video/259804261'
+    },
+    {
+        type: 'animation',
+        title: 'Dogfish',
+        imageUrl: '/img/dogfish.gif',
+        description: 'Dog pooping itself out',
+        thumbUrl: '/img/dogfish.gif'
+    },
+        // ** ILLUSTRATION **
+    {
+        type: 'animation',
+        title: 'Blue Girl',
+        imageUrl: '/img/blue-girl.png',
+        description: 'In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content',
+        thumbUrl: '/img/blue-girl.png'
+    },
+    {
+        type: 'illustration',
+        title: 'Zebra',
+        imageUrl: '/img/bullshittime.png',
+        description: 'Stretching down to eat?',
+        thumbUrl: '/img/bullshittime.png'
+    },
+        // ** WEB **
+    {
+        type: 'web',
+        title: 'DawnAyer.com',
+        imageUrl: '/img/dawn-ayer.png',
+        description: 'Commissioned by dawn ayer COMMUNICATIONS to design and develop a website promoting their services',
+        thumbUrl: '/img/thumbnails/dawn-ayer.png'
+    },
+]
+
+module.exports = projects;
