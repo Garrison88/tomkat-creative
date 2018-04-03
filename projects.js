e1764f7060560bde660b6aa815971730715eefa1
Garrison Siberry <garrison.siberry@gmail.com>
2018-04-03 19:42:54 +0000

    change projects.js naming convention

--- projects.js
+++ projects.js
@@ -2,111 +2,111 @@ let projects = {
     animation:[       
     {
         title: 'Jellyegg',
-        images: ['jellyegg.gif'],
+        imagePaths: ['jellyegg.gif'],
         description: 'Floating in space',
-        thumb: 'jellyegg.gif'
+        thumbPath: 'jellyegg.gif'
     },
     {
         title: 'Panda Robot',
-        images: ['panda-robot.gif'],
+        imagePaths: ['panda-robot.gif'],
         description: 'Panda robot being electrocuted',
-        thumb: 'panda-robot.gif'
+        thumbPath: 'panda-robot.gif'
     },
         {
         title: 'Dolphin',
-        images: ['anim_dolphin.gif'],
+        imagePaths: ['anim_dolphin.gif'],
         description: 'Baaaayyyybbyyy dolphin...!',
-        thumb: 'dolphin.gif'
+        thumbPath: 'dolphin.gif'
     },
         {
         title: 'Abstract Film',
-        images: ['sexy-film.gif'],
+        imagePaths: ['sexy-film.gif'],
         description: 'Abstract sexy gif',
-        thumb: 'sexy-film.png'
+        thumbPath: 'sexy-film.png'
     },
         {
         title: 'Powerstick',
         description: 'Wherever Pete goes, he is never without his trusty Powerstick. Don\'t get stuck without your portable devices. Carry power with you just like Pete!',
-        thumb: 'powerstick.png',
+        thumbPath: 'powerstick.png',
         videoEmbedUrls: ['youtube.com/embed/bsuD9j7QxKQ', 'youtube.com/embed/eTu6m1PpEI0', 'youtube.com/embed/oNGiLFF9dYY']
     },
         {
         title: 'Demo Reel',
         description: 'Demo reel',
-        thumb: 'demo-reel.png',
+        thumbPath: 'demo-reel.png',
         videoEmbedUrls: ['player.vimeo.com/video/259600005']
     },
         {
         title: 'Birthday Wishes',
         description: 'Birthday Wishes for JibJab Facebook App',
-        thumb: 'birthday-wishes.png',
+        thumbPath: 'birthday-wishes.png',
         videoEmbedUrls: ['player.vimeo.com/video/259804841']
     },
         {
         title: 'Enhanced Imagination',
         description: 'Animation spot for Enhanced Imagination',
-        thumb: 'enhanced-imagination.png',
+        thumbPath: 'enhanced-imagination.png',
         videoEmbedUrls: ['player.vimeo.com/video/259804372']
     },
         {
         title: 'Ground Zero',
         description: 'Commercial spot for Ground Zero (Toronto)',
-        thumb: 'ground-zero.png',
+        thumbPath: 'ground-zero.png',
         videoEmbedUrls: ['player.vimeo.com/video/259804261']
     }],
     illustration:[      
     {
         title: 'Berry Boat',
-        images: ['berry-boat-1.png', 'berry-boat-2.png'],
+        imagePaths: ['berry-boat-1.png', 'berry-boat-2.png'],
         description: 'Children\'s book',
-        thumb: 'berry-boat.png'
+        thumbPath: 'berry-boat.png'
     },
         {
         title: 'Handsome Man',
-        images: ['gerby-1.jpg', 'gerby-2.png'],
+        imagePaths: ['gerby-1.jpg', 'gerby-2.png'],
         description: 'Disney-fied',
-        thumb: 'gerby.png'
+        thumbPath: 'gerby.png'
     },
         {
         title: 'Abstract dancing girl',
-        images: ['abstract-girl.png'],
+        imagePaths: ['abstract-girl.png'],
         description: 'Colourful abstract naked dancing lady',
-        thumb: 'abstract-girl.png'
+        thumbPath: 'abstract-girl.png'
     },
         {
         title: 'Animals',
-        images: ['animals-1.png', 'animals-2.png'],
+        imagePaths: ['animals-1.png', 'animals-2.png'],
         description: 'Wild animals',
-        thumb: 'animals.png'
+        thumbPath: 'animals.png'
     }],
     web:[        
     {
         title: 'Barking Teacup',
-        images: ['barking-teacup-1.png', 'barking-teacup-2.png'],
+        imagePaths: ['barking-teacup-1.png', 'barking-teacup-2.png'],
         siteUrl: 'https://twitter.com/barkingteacup',
         description: 'Barking Teacup logo',
-        thumb: 'barking-teacup.png'
+        thumbPath: 'barking-teacup.png'
     },
         {
         title: 'Big World Small Budget',
-        images: ['bwsb.png'],
+        imagePaths: ['bwsb.png'],
         siteUrl: 'http://bigworldsmallbudget.com/',
         description: 'Big World Small Budget logo',
-        thumb: 'bwsb.png'
+        thumbPath: 'bwsb.png'
     },
         {
         title: 'Junk Removal Android App',
-        images: ['roi-1.png', 'roi-2.png', 'roi-3.png', 'roi-4.png', 'roi-5.png', 'roi-6.png', 'roi-7.png'],
+        imagePaths: ['roi-1.png', 'roi-2.png', 'roi-3.png', 'roi-4.png', 'roi-5.png', 'roi-6.png', 'roi-7.png'],
         siteUrl: 'https://www.ridofittoronto.com/',
         description: 'Logistics app created for a junk removal company in Toronto. Connects to Google Firebase database and features user authentication, real-time data-sync, and Material Design',
-        thumb: 'roi.png'
+        thumbPath: 'roi.png'
     },
         {
         title: 'DawnAyer.com',
-        images: ['dawn-ayer-1.png', 'dawn-ayer-2.png', 'dawn-ayer-3.png'],
+        imagePaths: ['dawn-ayer-1.png', 'dawn-ayer-2.png', 'dawn-ayer-3.png'],
         siteUrl: 'http://dawnayer.com/',
         description: 'Commissioned by dawn ayer COMMUNICATIONS to design and develop a website promoting their services',
-        thumb: 'dawn-ayer.png'
+        thumbPath: 'dawn-ayer.png'
     }]
 };
 
